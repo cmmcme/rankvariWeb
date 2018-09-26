c9660081ca452f1c39ce586dc5ee7ccd7e0c12f0
pppgod <strangeyun@gmail.com>
2018-09-26 22:31:02 +0900

    add chart view

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,43 @@
 var express = require('express');
 var router = express.Router();
+const MongoClient = require('mongodb').MongoClient;
+
+const URL = 'mongodb://192.168.1.8:27017/';
+const dbName = 'rankvari';
+const collectionName = 'wordjavavari';
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+
+  MongoClient.connect(URL, function(err, client) {
+    if(err) {
+      console.error(err);
+      return ;
+    }
+
+    console.log("Connected successfully to server");
+
+
+    const x = [];
+    const pv = [];
+
+    const db = client.db(dbName);
+    db.collection(collectionName).find().sort({count: -1}).limit(20).toArray(function(err, docs) {
+      if(err) {
+        console.error(err);
+      }
+
+      docs.map(function(doc) {
+        x.push({value: doc.vari});
+        pv.push({value: doc.count});
+      });
+
+      console.log(docs);
+    });
+
+    client.close();
+    res.render('index', { title: 'Express', x: x, pv: pv });
+  });
 });
 
 module.exports = router;
